03446c3901aa9d960820f9f48c5d49a3a57492f4
ltan2 <ltan2@zagmail.gonzaga.edu>
2024-12-22 14:53:02 -0600

    complete locating restr

--- locating_restrictions.py
+++ locating_restrictions.py
@@ -1,22 +1,18 @@
-def reverse_complement(seq):
-    reverse_seq = ""
-    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-    for base in seq:
-        reverse_seq += complement[base]
-    return reverse_seq
-
 def find_reverse_palindromes(dna, min_len=4, max_len=12):
     results = []
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-    for base in seq:
     for i in range(len(dna)):
         for length in range(min_len, max_len + 1):
             if i + length > len(dna):
                 break
             substring = dna[i:i + length]
-            for base in seq:
-        reverse_seq += complement[base]
-            if substring == reverse_complement(substring):
+            reversed_str = ""
+            for j in range(len(substring)):
+                if complement[substring[j]] == substring[len(substring) - 1 - j]:
+                    reversed_str = complement[substring[j]] + reversed_str
+                else:
+                    break
+            if substring == reversed_str:
                 results.append((i + 1, length))
     return results
 
